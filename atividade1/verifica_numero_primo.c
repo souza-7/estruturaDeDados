5c7cb8f692702130fc56837532b699fd3c0815ea
souza-7 <84939200+souza-7@users.noreply.github.com>
2023-09-04 09:36:30 -0300

    Add files via upload

---
+++ atividade1/verifica_numero_primo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int ehPrimo(int numero) {
+    if (numero <= 1) {
+        return 0; 
+    }
+
+    for (int i = 2; i * i <= numero; i++) {
+        if (numero % i == 0) {
+            return 0; 
+        }
+    }
+
+    return 1; 
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <numero>\n", argv[0]);
+        return 1;
+    }
+
+    int numero = atoi(argv[1]);
+
+    if (ehPrimo(numero)) {
+        printf("1 - O número %d é primo.\n", numero);
+    } else {
+        printf("0 - O número %d não é primo.\n", numero);
+    }
+
+    return 0;
+}
